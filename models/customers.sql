31873d2a2b4d9411161849f2ba6789e174659047
Javier Matias-Cabrera <jmatias@thoughtworks.com>
2022-10-13 17:44:33 -0400

    Before writing tests.

--- models/customers.sql
+++ models/customers.sql
@@ -1,55 +1,29 @@
-with customers as (
+with customers as (select *
+                   from {{ ref('stg_customers') }}),
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
+     orders as (select *
+                from {{ ref('stg_orders') }}),
 
-    from dbt.coned.jaffle_shop_customers
+     customer_orders as (select customer_id,
 
-),
+                                min(order_date) as first_order_date,
+                                max(order_date) as most_recent_order_date,
+                                count(order_id) as number_of_orders
 
-     orders as (
+                         from orders
 
-         select
-             id as order_id,
-             user_id as customer_id,
-             order_date,
-             status
+                         group by 1),
 
-         from dbt.coned.jaffle_shop_orders
+     final as (select customers.customer_id,
+                      customers.first_name,
+                      customers.last_name,
+                      customer_orders.first_order_date,
+                      customer_orders.most_recent_order_date,
+                      coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
-     ),
+               from customers
 
-     customer_orders as (
+                        left join customer_orders using (customer_id))
 
-         select
-             customer_id,
-
-             min(order_date) as first_order_date,
-             max(order_date) as most_recent_order_date,
-             count(order_id) as number_of_orders
-
-         from orders
-
-         group by 1
-
-     ),
-
-     final as (
-
-         select
-             customers.customer_id,
-             customers.first_name,
-             customers.last_name,
-             customer_orders.first_order_date,
-             customer_orders.most_recent_order_date,
-             coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-
-         from customers
-
-                  left join customer_orders using (customer_id)
-
-     )
-
-select * from final
+select *
+from final

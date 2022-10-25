9c07c697d1350ed3322567fa509aff6a443e15a8
Javier Matias-Cabrera <jmatias@thoughtworks.com>
2022-10-25 02:04:23 -0400

    wip

--- models/global_sales/countries_by_total_sales.sql
+++ models/global_sales/countries_by_total_sales.sql
@@ -1,6 +1,17 @@
+with cities as (select * from {{source('global_sales', 'cities')}}),
+     transactions as (select * from {{source('global_sales', 'transactions')}}),
+
+     countries as (select count(country) as count,
+                          country,
+                          avg(amount)    as avg_amount,
+                          sum(amount)    as total_amount,
+                          median(amount) as median_amount
+                   from cities
+                            left join transactions
+                                      on cities.city_id = transactions.city_id
+                   group by country)
 
 
-select count(country) as count, country, avg(amount) as avg_amount, sum(amount) as total_amount
-from {{source('global_sales', 'cities')}}
-         left join {{source('global_sales', 'transactions')}} on cities.city_id = transactions.city_id
-group by country sort by total_amount desc;
+select *
+from countries
+order by total_amount desc;
